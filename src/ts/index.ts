4cdf749fc991f54a4983f49cb504c134de754f31
Sam Noh <samnoh21@outlook.com>
2019-08-07 14:41:22 +1200

    mod - clean up

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,10 +1,11 @@
 import { UserList } from './views';
-import { Collection, UserProps, User } from './models';
+import { User } from './models';
 
 const users = User.buildUserCollection();
-console.log(users);
+
 users.on('change', () => {
     const root = document.getElementById('root');
+
     if (root) {
         new UserList(root, users).render();
     }
